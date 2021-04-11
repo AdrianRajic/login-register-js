22f0f3a7354980f10118a3a699ca51a46c06105c
AdrianRajic <71076593+AdrianRajic@users.noreply.github.com>
2021-04-11 20:32:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 var Email = document.getElementById("email-address")
 var Password = document.getElementById("password")
 var Email,Password
+
+function store() {
+
+    localStorage.setItem("Email", email-address.value)
+    localStorage.setItem("Password", password.value)
+}
