f0f1464ef48684862b0cb0b91ff10de681aa3c50
AdrianRajic <71076593+AdrianRajic@users.noreply.github.com>
2021-04-29 10:50:12 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-var Email = document.getElementById("email-address")
-var Password = document.getElementById("password")
-var Email,Password
-
-function store() {
-
-    localStorage.setItem("Email", email-address.value)
-    localStorage.setItem("Password", password.value)
-}
